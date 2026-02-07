c3abe30ef7ce2bcbfb7f0303765a0fd022850f5f
Sayantan  sengupta <sayantansengupta@Sayantans-MacBook-Pro.local>
2026-02-07 13:00:18 +0100

    Added test step in CI/CD pipeline

--- test/test_app.py
+++ test/test_app.py
@@ -6,3 +6,9 @@ Created on Fri Feb  6 11:54:00 2026
 @author: sayantansengupta
 """
 
+# tests/test_app.py
+from src.predict import predict
+
+def test_prediction():
+    result = predict([5.1, 3.5, 1.4, 0.2])
+    assert result in [0, 1, 2]
